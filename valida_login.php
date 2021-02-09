1d8297f5201dadb0967cbae05351b07e67edfb93
guibatista <ytb.batista99@gmail.com>
2021-02-09 11:33:10 -0300

    AULA07

--- valida_login.php
+++ valida_login.php
@@ -7,13 +7,41 @@
 
     $senha=$_POST['senha'];
 
-    echo "USUARIO: ".$usuario;
-    echo "<br>";
-    echo "SENHA: ".$senha;
+    $hashsenha=trim(password_hash($senha, PASSWORD_DEFAULT));
 
+   $sql= "SELECT senha * FROM tbl_usuarios WHERE usuario='$usuario' ";
 
+   include_once "conexao.php" 
 
+// executando //
 
+ $resultado = mysqli_query($conn, $sql);
 
+// verificando se encontrou o usuaio cadastrado na base de dados //
+
+if($campo=mysqli_fetch_array($resultado))
+{
+    // enontrou um usuario cadastrado //
+// caso tenha encontrado vamos com para as senhas // 
+if($campo['senha']==password_verify($senha, $hashsenha)  )
+{// a senha está correta //
+// usuario esta com autorização para entrar //
+?>
+<script>
+    alert ("USUARIO LOGADO")    
+    </script>
+
+    <?php
+}
+
+else {
+    {?>
+    <script>
+    alert("USUÁRIO NÃO CADASTRADO")
+    </script>
+    <?php
+}
+}
+}
 
 ?>
